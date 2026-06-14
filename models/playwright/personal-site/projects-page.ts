4e3e1f9d27bcc6df97fabc2a01f592e5ba95a499
Sarah Crowe <sarahncrowe@gmail.com>
2026-06-14 13:56:48 -0400

    Revert: undo unauthorized direct commit to main

--- models/playwright/personal-site/projects-page.ts
+++ models/playwright/personal-site/projects-page.ts
@@ -18,7 +18,7 @@ class ProjectsPage {
   constructor(page: Page) {
     this.page = page;
     this.heading = page.getByRole('heading', { level: 1 });
-    this.description = page.getByTestId('repo-description');
+    this.description = page.getByTestId('repo-header').locator('p');
     this.playwrightTab = page.getByTestId('tab-playwright');
     this.testcafeTab = page.getByTestId('tab-testcafe');
     this.cypressTab = page.getByTestId('tab-cypress');
